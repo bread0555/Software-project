ab2d76dd349664b3ed77a96e0a7ef0a7d76a1a8a
Brandon Lin <92833005+bread0555@users.noreply.github.com>
2025-07-20 14:14:38 +0000

    added test case files, started introducing class of main program

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print("Hello World")
+
+class CodeBlock:
+  def __init__(self, block_type):
+    self.block_type = block_type
+
+  def to_python(self):
+    if self.block_type == "IF":
+      pass
+    if self.block_type == "ELSE":
+      pass
