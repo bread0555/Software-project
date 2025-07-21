5e8edd8d08cc8aed94a6f597428f1c20c653a457
Brandon Lin <92833005+bread0555@users.noreply.github.com>
2025-07-21 16:23:47 +0000

    added comments of what to do later, starting general plan of code

--- main.py
+++ main.py
@@ -11,11 +11,20 @@ for line in lines:
   
 
 class CodeBlock:
-  def __init__(self, block_type):
-    self.block_type = block_type
+  def __init__(self, input, indent_level = 0):
+    self.input = input
+    self.output = output
+
+  def analyse(self)):
+    # analyse the line given, stores the first word in a variable
 
   def to_python(self):
+
+    # need something to check how long the indent goes for, and send in the input into the recursive input
     if self.block_type == "IF":
       pass
     if self.block_type == "ELSE":
       pass
+    if self.variable_name == "while":
+      CodeBlock(self.input) # recurrsively create new classes to solve the problem of code idented far inside
+      
