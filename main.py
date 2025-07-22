06ff62b2a8d8c50e3ce1a8e17e090f7c59636c40
Brandon Lin <92833005+bread0555@users.noreply.github.com>
2025-07-22 10:44:32 +0000

    starting to create algorithm to handle indented lines of code

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-print("Hello World")
-
 lines = []
 with open("demo/if_else.txt") as f:
   for line in f:
     lines.append(line.strip("\n"))
 
-print(lines)
 
-for line in lines:
-  
 
 class CodeBlock:
   def __init__(self, input, indent_level = 0):
     self.input = input
     self.output = output
+    self.indent_level = indent_level
 
   def analyse(self)):
-    # analyse the line given, stores the first word in a variable
+    if self.input[0].startswith(" " + " " * self.indent_level):
+      
+    else:
+      self.name = self.input[0].strip(" ")
+      return
+      
+    # analyse the line given, storesthe first word in a variable
 
   def to_python(self):
 
@@ -27,4 +29,7 @@ class CodeBlock:
       pass
     if self.variable_name == "while":
       CodeBlock(self.input) # recurrsively create new classes to solve the problem of code idented far inside
+
+
+c = CodeBlock(lines)
       
